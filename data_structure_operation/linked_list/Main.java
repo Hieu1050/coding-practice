d1e038934a2ff91f905c9ecb8df7603e311bf760
Hieu1050 <vuchihieu1050@gmail.com>
2022-02-22 23:09:59 +0900

    ll

--- data_structure_operation/linked_list/Main.java
+++ data_structure_operation/linked_list/Main.java
@@ -1,6 +1,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("Hello world!");
+        ListNode n1 = new ListNode (1);
+        ListNode n2 = new ListNode (2);
+        ListNode n3 = new ListNode (3);
+        ListNode n4 = new ListNode (4);
+        ListNode n5 = new ListNode (5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        printLinkedList (n1);
     }
+    
+    public static void printLinkedList (ListNode node){
+        if  (node != null){
+            System.out.printf("%d", node.val ); 
+            if (node.next != null){
+                System.out.printf("->");
+                printLinkedList(node.next);
+            }
+            else {System.out.println ();}
+        }
+    }
+
 }
