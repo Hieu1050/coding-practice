e5fa9c788f7e4c73ec32355cda4f1a66dadbf12d
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-01 20:59:14 +0900

    reverse linked list

---
+++ data_structure_operation/linked_list/ReverseLinkedList.java
@@ -0,0 +1,12 @@
+public class ReverseLinkedList {
+    public static ListNode reverseListNode (ListNode head){
+        ListNode prev_node = null;
+        while (head != null){
+            ListNode next_node = head.next;
+            head.next = prev_node;
+            prev_node = head;
+            head = next_node;
+        }
+        return prev_node;
+    }
+}
