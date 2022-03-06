4871066e479befc8237637e365db6e6d5bb18413
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-06 16:45:01 +0900

    1-6 String Compression

---
+++ set1-string/StringCompression.java
@@ -0,0 +1,36 @@
+public class StringCompression {
+    public static void main(String[] args) {
+        System.out.println (Solution("abcd"));
+    }
+    static String Solution (String s){
+        if (s.length()== 0) {return s;}
+        int s_len = s.length();
+        if (s_len < compressLength(s, s_len)){return s;}
+
+        StringBuilder res = new StringBuilder();
+        int count =0;
+        for (int i = 0; i < s_len ; i++) {
+            count ++;
+            // if the next char is not the same OR cur char is the last char
+            if (i == s_len-1 || s.charAt(i+1) != s.charAt(i)) {
+                // add curr char to res
+                res.append(s.charAt(i));
+                // add count to res
+                res.append(Integer.toString(count));
+                // reset count
+                count = 0;
+            }
+        }
+        return res.toString();
+
+    }
+    static int compressLength (String s, int s_len){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s_len-1 || s.charAt(i+1) != s.charAt(i)){
+                count += 2;
+            }
+        }
+        return count;
+    }
+}
