0a2cb937fd08958f253561dd326e26d406703089
Hieu1050 <vuchihieu1050@gmail.com>
2022-03-22 08:09:34 +0900

    shorter recursive preorder traversal

---
+++ set3-binary-tree/IterativePostorderTraversal.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class IterativePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // left - right - root
+        // base case
+        // while curr root is not null
+            // stack push (root)
+            // stack push (right)
+            // stakc push (left)
+            
+    }
+}
--- set3-binary-tree/RecursivePreorderTraversal.java
+++ set3-binary-tree/RecursivePreorderTraversal.java
@@ -3,15 +3,15 @@ import java.util.List;
 
 public class RecursivePreorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        helper (root, res);
+        List<Integer> res= new ArrayList<>();
+        helper(root, res);
         return res;
     }
-    static void helper (TreeNode node, List<Integer> list){
+    static void helper (TreeNode node, List<Integer> lst){
         if (node != null){
-            list.add(node.val);
-            helper (node.left, list);
-            helper (node.right, list);
+            lst.add(node.val);
+            helper(node.left, lst);
+            helper(node.right, lst);
         }
     }
 }
